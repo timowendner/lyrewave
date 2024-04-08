abacdbbf959e3efbc1fd703c86dd266910972500
Timo Wendner <timo.wendner@gmx.net>
2024-04-09 00:33:17 +0200

    model import changes

---
+++ src/lyrewave/__init__.py
@@ -0,0 +1,6 @@
+from .main import run
+from .dataloader import get_dataloaders, AudioDataset
+from .diffusion import Diffusion
+from .model import UNet
+from .utils import open_config
+from .train import train_network
